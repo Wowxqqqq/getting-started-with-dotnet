3df66b6db68489709c5f4559828c99dfa44eaad2
Wowxqqqq <tristan@osidon.com>
2024-06-25 12:29:20 +0200

    The necessary scripts and file added.

---
+++ Choco_Install_Apps.ps1
@@ -0,0 +1,19 @@
+# The following code provides the commands to install the relevant apps using chocotely via Powershell.
+
+# Install Visual Studio Community 2022
+choco install visualstudio2022community -y
+
+# Install Docker Desktop
+choco install docker-desktop -y
+
+# Install Azure Data Studio
+choco install azure-data-studio -y
+
+# Install Git and Git Bash
+choco install git -y
+
+# Install Heroku CLI
+choco install heroku-cli -y
+
+# Install .NET Core CLI
+choco install dotnetcore-sdk -y
